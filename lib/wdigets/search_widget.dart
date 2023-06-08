2b65d31a48b9ac3a469f10be9433b3cab4f06503
MightyM17 <bavishimithil@gmail.com>
2023-06-08 15:40:53 +0530

    lib/wdigets/search_widget.dart: New Search UI

--- lib/wdigets/search_widget.dart
+++ lib/wdigets/search_widget.dart
@@ -13,20 +13,27 @@ class SearchWidget extends StatelessWidget {
       padding: const EdgeInsets.all(20),
       child: Container(
         decoration: BoxDecoration(
-            color: const Color(0xffF1F1F1),
+            color: const Color(0x85FFFFFF),
             borderRadius: BorderRadius.circular(10)),
         padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 20),
-        child: Row(
-          children: [
-            const Icon(Icons.search, color: Color(0xff878787)),
-            const SizedBox(
-              width: 10,
-            ),
-            Text(
-              'Search...',
-              style: latoRegular16.copyWith(color: const Color(0xff878787)),
-            ),
-          ],
+        child: IntrinsicHeight(
+          child: Row(
+            children: [
+              const Icon(Icons.search, color: Colors.white),
+              const VerticalDivider(
+                thickness: 1,
+                color: Colors.white,
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              Text(
+                'Search',
+                style: latoRegular16.copyWith(color: Colors.white),
+              ),
+
+            ],
+          ),
         ),
       ),
     );
